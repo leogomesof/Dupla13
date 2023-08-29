33b7a267e129ff6d51baf9dade2232fd474aa133
leogomesof <leogomesof@icloud.com>
2023-08-29 20:46:24 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Drawing;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+
+namespace fixacao_fundamentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("O salário final é R$: " + Aumento.Calculo());
+            Console.Write("Aperte enter para sair");
+            Console.Read();
+        }
+    }
+}
